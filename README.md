f13743831b7619f788055c9b3f7b0fee4d93a47a
Ruchi Agarwal <ruchi.agarwalcse@gmail.com>
2015-05-16 22:01:29 -0700

    Lab 3 Final Updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-CMPE 273 Lab 3
+cmpe273-lab6
+============
+
+CMPE 273 Lab 6 Baseline
---
+++ client/bin/client.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#java -jar target/client-0.0.1-SNAPSHOT.jar
+java -jar target/client-0.0.1-SNAPSHOT.jar
